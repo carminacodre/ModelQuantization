f0a2f72210fa6f4ef0bef3408e056a0b03aae4ec
Carmina Codre <carminacodre@gmail.com>
2018-05-22 23:36:27 +0300

    fixed shape

--- quantization/operations.py
+++ quantization/operations.py
@@ -22,23 +22,20 @@ optimize_quantize_weights_trans = ['strip_unused_nodes(type=float, shape="1,'
                                    'quantize_weights']
 
 def get_optimizing_for_deployment_for_shape(shape):
-    return ['strip_unused_nodes(type=float, shape="1,'
-            + str(shape) + ',' + str(shape) + ',3")',
+    return ['strip_unused_nodes(type=float, shape=' + shape + ')',
             'remove_nodes(op=Identity, op=CheckNumerics)',
             'fold_constants(ignore_errors=true)',
             'fold_batch_norms',
             'fold_old_batch_norms']
 
 def get_fix_missing_kernel_errors_for_shape(shape):
-    return ['strip_unused_nodes(type=float, shape="1,'
-            +str(shape)+','+str(shape)+',3")',
+    return ['strip_unused_nodes(type=float, shape=' + shape + ')',
             'fold_constants(ignore_errors=true)',
             'fold_batch_norms',
             'fold_old_batch_norms']
 
 def get_optimize_quantize_weights_for_shape(shape):
-    return ['strip_unused_nodes(type=float, shape="1,'
-            +str(shape)+','+str(shape)+',3")',
+    return ['strip_unused_nodes(type=float, shape=' + shape + ')',
             'fold_constants(ignore_errors=true)',
             'fold_batch_norms',
             'fold_old_batch_norms',
